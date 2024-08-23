d745f058619d6b63147dbc437fbf00c6ebb50729
sahashiharshit <harshitsah96@gmail.com>
2024-08-23 17:41:12 +0530

    Update:getElementById and getElementsByClassName

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("I love coding.");
+let mainheading = document.getElementById("main-heading");
+let header = document.getElementById("header");
+let basketheading = document.getElementById("basket-heading");
+let thanks = document.getElementById("thanks");
+mainheading.textContent ="Fruit World"
+mainheading.style.color = "orange";
+header.style.backgroundColor ="green";
+header.style.borderBottom = " 3px solid orange";
+
+basketheading.style.color="green";
+thanks.innerHTML="<p>Please visit us again</p>";
+
+
+
+const fruit = document.getElementsByClassName("fruit");
+fruit[2].style.backgroundColor ="yellow";
+
+for(let i=0;i<fruit.length;i++){
+fruit[i].style.fontWeight="bold";
+}
